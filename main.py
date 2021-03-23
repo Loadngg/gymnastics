4df52886812044802eb376e811d3eecf5828a586
Loadngg <averinj@mail.ru>
2021-03-23 13:10:58 +0300

    Еще раз переписан дизайн кнопок, добавлена инициализация pyttsx3

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ from kivy.app import App
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.core.window import Window
 import exercising as ex
+import pyttsx3
 
 kivy.require('2.0.0')
+
+tts = pyttsx3.init()
+tts.setProperty('rate', 250)
+tts.setProperty('volume', 1)
 
 
 class MenuScreen(Screen):
@@ -20,13 +25,14 @@ class MainScreen(Screen):
     def change_ex_next(self, exercising):
         if self.i == 24:
             self.i = 0
+            sm.current = 'end'
         self.i += 1
         self.text = u'{}'.format(ex.exercising_list[self.i])
         exercising.text = self.text
 
     def change_ex_prev(self, exercising):
         if self.i == 0:
-            pass
+            sm.current = 'menu'
         self.i -= 1
         self.text = u'{}'.format(ex.exercising_list[self.i])
         exercising.text = self.text
@@ -38,6 +44,7 @@ class EndingScreen(Screen):
 
 class MyApp(App):
     def build(self):
+        global sm
         sm = ScreenManager()
         sm.add_widget(MenuScreen(name='menu'))
         sm.add_widget(MainScreen(name='main'))
